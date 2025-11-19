098f2e82f6af8b082bf284dbcb44f4ef2e90c66d
Klaus <rafaelrodrigues1943@gmail.com>
2025-11-19 18:56:09 -0300

    Delete JS directory

--- JS/constants.js
+++
@@ -1,8 +0,0 @@
-/* js/constants.js — Constantes */
-
-export const CHANNEL_NAME = 'el_shaday_channel_v1';
-export const LS_ORDERS_KEY = 'el_shaday_orders_v1';
-export const LS_PRODUCTS_KEY = 'el_shaday_products_v1';
-export const LS_CART_KEY = 'el_shaday_cart_v1';
-export const LS_DRAFTS_KEY = 'el_shaday_drafts_v1';
-export const DRAFT_ID_KEY = 'el_shaday_client_id_v1';
